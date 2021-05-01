64b6641a8d25b8dea73b475f35c1fae05fc145a3
wakkihaya <wakkihaya@gmail.com>
2021-05-01 16:36:20 -0700

    update: blog draft

--- pages/blog/sample.tsx
+++ pages/blog/sample.tsx
@@ -1,26 +1,39 @@
-import Header from "../../src/components/Header";
-import Footer from "../../src/components/Footer";
+import Header from "../../src/components/Header"
+import Footer from "../../src/components/Footer"
+import draft from "../../src/blog-draft/sample"
+
+import React from "react"
+import ReactMarkdown from "react-markdown"
+import { render } from "react-dom"
 
 //TODO: templeteでまとめたい。
 const sample: React.FC = () => {
+  const title = draft.title
+  const png = draft.png
+  const date = draft.date
+  const markdown = draft.markdown
+
   return (
     <>
       <Header />
       <div className="blog-content">
         <div className="blog-content--top">
-          <div className="blog-content--top-pic">{/* TODO: show pic */}</div>
-          <div className="blog-content--top-date">Date: TODO: </div>
+          <div className="blog-content--top-pic">
+            <img src={png} />
+          </div>
+          <div className="blog-content--top-date">{date} </div>
         </div>
         <div className="blog-content--draft">
-          <div className="blog-content--draft-title">
-            title{/* TODO: show title */}
+          <div className="blog-content--draft-title">{title}</div>
+          <div className="blog-content--draft-text">
+            <ReactMarkdown children={markdown} />
           </div>
-          <div className="blog-content--draft-text">text //TODO:</div>
         </div>
       </div>
       <Footer />
     </>
   );
 };
+
 
 export default sample;
---
+++ src/blog-draft/sample.ts
@@ -0,0 +1,11 @@
+const markdown = `This is test sample markdown.`
+
+const title = 'sample title'
+
+const subtitle = 'test test'
+
+const date = 'April 4'
+
+const png = '~/static/images/sample.png'
+
+export default { title, subtitle, date, png, markdown }
