8684716a8ad83fb8d580f269df406a5d1606e8fb
wakkihaya <wakkihaya@gmail.com>
2021-05-01 22:21:20 -0700

    update: add brain active graph

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,23 +2,26 @@ import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
-    <header className="header">
-      <div className="header--logo">NeurotechJP</div>
-      <div className="header--index">
-        <div className="header--index-blog">
-          <Link href="/">Blog</Link>
+    <div className="header-container">
+      <header className="header">
+        <div className="header--logo">NeurotechJP</div>
+        <div className="header--index">
+          <div className="header--index-blog">
+            <Link href="/">Blog</Link>
+          </div>
+          <div className="header--index-about">
+            <Link href="/about">About NeurotechJP</Link>
+          </div>
         </div>
-        <div className="header--index-about">
-          <Link href="/about">About NeurotechJP</Link>
+        <div className="header--lang">
+          <div className="header--lang-en">EN/</div>
+          {/* TODO: Change to English */}
+          <div className="header--lang-JP">JP</div>
+          {/* TODO: Change to Japanese */}
         </div>
-      </div>
-      <div className="header--lang">
-        <div className="header--lang-en">EN/</div>
-        {/* TODO: Change to English */}
-        <div className="header--lang-JP">JP</div>
-        {/* TODO: Change to Japanese */}
-      </div>
-    </header>
+      </header>
+      <img src="/brain_wave.png" className="header-container--brain-wave" />
+    </div>
   );
 };
 
