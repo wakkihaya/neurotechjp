939bbc0d8b7b935befef6faaf2fa9e02fb209f8e
wakkihaya <wakkihaya@gmail.com>
2021-06-03 16:28:26 +0900

    update: og for slide

--- src/pages/jp/resources/basic-theories-of-neurotechnology.tsx
+++ src/pages/jp/resources/basic-theories-of-neurotechnology.tsx
@@ -1,9 +1,23 @@
 import Header from "~components/Header";
 import Footer from "~components/Footer";
+import { Meta } from "~components/Meta";
 
 const BasicTheoriesOfNeurotechnology: React.FC = () => {
   return (
     <>
+      <Meta
+        meta={{
+          title: "Basic Theories of Neurotechnology | NeurotechJP",
+          desc:
+            "Neurotechnologyの基礎知識をまとめたスライド資料です。" +
+            "1. このエリアの全体像(市場規模など)" +
+            "2.基礎知識(Neurotechnologyの種類、脳科学基礎、ソフトウェアエンジニアリング)" +
+            "3. ニューロテクノロジーを用いたユースケース",
+          link: "https://neurotechjp.com/jp/resources/basic-theories-of-neurotechnology/",
+          image:
+            "https://neurotechjp.com/resources/slide-basic-theory-neurotech.jpg",
+        }}
+      />
       <Header isBgTransparent={false} />
       <div className="resources-item">
         <div className="resources-item--main">
--- src/pages/resources/basic-theories-of-neurotechnology.tsx
+++ src/pages/resources/basic-theories-of-neurotechnology.tsx
@@ -1,9 +1,23 @@
 import Header from "~components/Header";
 import Footer from "~components/Footer";
+import { Meta } from "~components/Meta";
 
 const BasicTheoriesOfNeurotechnology: React.FC = () => {
   return (
     <>
+      <Meta
+        meta={{
+          title: "Basic Theories of Neurotechnology | NeurotechJP",
+          desc:
+            "This slide is about the basic theories of Neurotechnology. It shows" +
+            "1.  An overview of this area with a market value." +
+            "2. Basic knowledge including types of neurotechnologies, basics of neuroscience, and software engineering." +
+            "3.Use cases with neurotechnologies",
+          link: "https://neurotechjp.com/resources/basic-theories-of-neurotechnology/",
+          image:
+            "https://neurotechjp.com/resources/slide-basic-theory-neurotech.jpg",
+        }}
+      />
       <Header isBgTransparent={false} />
       <div className="resources-item">
         <div className="resources-item--main">
