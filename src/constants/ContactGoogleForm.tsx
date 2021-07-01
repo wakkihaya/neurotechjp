7f9831c3af8ec4f4be342ee4b3f7d5d8de1a0b21
wakkihaya <wakkihaya@gmail.com>
2021-07-01 09:07:54 +0900

    clean: remove unused file

--- src/constants/ContactGoogleForm.tsx
+++
@@ -1,7 +0,0 @@
-export const ContactGoogleForm = {
-  action:
-    "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdaWFJEaQVkgEFlgkRudAo3DOUgPOnxB8qycJ2Z3XUtllydaQ/formResponse",
-  name: "entry.1315928669",
-  mail: "entry.1439350300",
-  message: "entry.339139323",
-};
