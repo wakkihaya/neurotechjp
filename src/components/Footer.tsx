5e7bb4a05b29b2eae1ecc3ac78f871956f1a4f9f
wakkihaya <wakkihaya@gmail.com>
2021-07-09 15:37:43 +0900

    add: cookie component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import CookieConsent from "react-cookie-consent";
 
 const Footer: React.FC = () => {
   return (
-    <>
-      <footer className="footer">@2021 NeurotechJP</footer>
-    </>
+    <div className="footer">
+      <footer className="footer--content">@2021 NeurotechJP</footer>
+
+      <CookieConsent
+        location="bottom"
+        buttonText="I understand"
+        cookieName="cookie-name"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
+    </div>
   );
 };
 
