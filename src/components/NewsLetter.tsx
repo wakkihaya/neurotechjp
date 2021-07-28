2c8b8d483b12cf06675f8100ee4af04cc0e1c324
wakkihaya <wakkihaya@gmail.com>
2021-07-28 22:48:19 +0900

    update: newsletter

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -1,10 +1,57 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 import React from "react";
 
+const CustomForm = ({ status, message, onValidated }) => {
+  let email;
+  const submit = () =>
+    email &&
+    email.value.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email.value,
+    });
+
+  return (
+    <>
+      {status != "error" ? (
+        <>
+          {status !== "success" ? (
+            <>
+              <input
+                ref={node => (email = node)}
+                type="email"
+                placeholder="Email"
+              />
+              {status === "sending" ? (
+                <div className="footer--newsletter-sending">Sending...</div>
+              ) : (
+                <button onClick={submit}>Subscribe</button>
+              )}
+            </>
+          ) : (
+            <div className="footer--newsletter-success">You're subscribed!</div>
+          )}
+        </>
+      ) : (
+        <div className="footer--newsletter-error">Error!</div>
+      )}
+    </>
+  );
+};
 const NewsLetterMailForm: React.FC = () => {
   const url = process.env.MAILCHIMP_URL || "";
 
-  return <MailchimpSubscribe url={url} />;
+  return (
+    <MailchimpSubscribe
+      url={url}
+      render={({ subscribe, status, message }) => (
+        <CustomForm
+          status={status}
+          message={message}
+          onValidated={formData => subscribe(formData)}
+        />
+      )}
+    />
+  );
 };
 
-//TODO: header かFotterに埋め込む
+export default NewsLetterMailForm;
