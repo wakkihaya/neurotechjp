4b05a9e2520b7d8e61b6d212a2a3f2f2331a33ff
wakkihaya <wakkihaya@gmail.com>
2021-08-01 09:54:47 +0900

    update: disable subtitle in meta

--- src/pages/jp/index.tsx
+++ src/pages/jp/index.tsx
@@ -73,7 +73,9 @@ const BlogItem: React.FC<{ post: PostData }> = props => {
         <div className="blog-item--container">
           <div className="blog-item--container-title">{post.title}</div>
           <div className="blog-item--container-category">{post.category}</div>
-          <div className="blog-item--container-subtitle">{post.subtitle}</div>
+          <div className="blog-item--container-subtitle">
+            {post.description}
+          </div>
           <div className="blog-item--container-readmore">READ MORE..</div>
         </div>
         <div className="blog-item--date">{post.datePublished}</div>
