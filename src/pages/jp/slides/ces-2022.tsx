4c277ab922836e52cfd0dedf12a1e453227e09db
wakkihaya <wakkihaya@gmail.com>
2022-05-10 10:21:18 +0900

    fix: style

--- src/pages/jp/slides/ces-2022.tsx
+++ src/pages/jp/slides/ces-2022.tsx
@@ -25,6 +25,7 @@ const CES2022: React.FC = () => {
           <br />
           購入にご興味ある方/お話から聞いてみたい方は、下記のフォームからご連絡下さい！
           <br />
+          <br />
           単価価格：30万円
         </div>
         <div className="slides-item--button">
